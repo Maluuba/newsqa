a002d4cdaf2e91e624624b9a78f5d3cf7a7e91ee
Justin Harris <justin.harris@maluuba.com>
2016-12-22 12:44:54 -0500

    split: Filter out when no answer was picked because those weren't used in the original paper. resolves #5

--- maluuba/newsqa/split_dataset.py
+++ maluuba/newsqa/split_dataset.py
@@ -5,6 +5,12 @@ import os
 import pandas as pd
 from tqdm import tqdm
 
+try:
+    # Prefer a more specific path.
+    from maluuba.newsqa.data_processing import NewsQaDataset
+except:
+    from data_processing import NewsQaDataset
+
 logging.basicConfig(level=logging.INFO)
 
 dir_name = os.path.dirname(os.path.abspath(__file__))
@@ -20,11 +26,9 @@ parser.add_argument('--output_dir', default=default_output_dir,
                          % default_output_dir)
 args = parser.parse_args()
 
-original = pd.read_csv(args.dataset_path,
-                       encoding='utf-8',
-                       na_values=dict(question=[]),
-                       keep_default_na=False)
+original = NewsQaDataset.load_combined(args.dataset_path)
 
+logging.info("Loading story ID's split.")
 train_story_ids = set(pd.read_csv(os.path.join(dir_name, 'train_story_ids.csv'))['story_id'].values)
 dev_story_ids = set(pd.read_csv(os.path.join(dir_name, 'dev_story_ids.csv'))['story_id'].values)
 test_story_ids = set(pd.read_csv(os.path.join(dir_name, 'test_story_ids.csv'))['story_id'].values)
@@ -33,10 +37,16 @@ train_data = []
 dev_data = []
 test_data = []
 
-for _, row in tqdm(original.iterrows(), total=len(original),
-                   mininterval=2, unit_scale=True, unit=" questions",
-                   desc="Splitting data"):
-    story_id = row['story_id']
+for row in tqdm(original.itertuples(), total=len(original),
+                mininterval=2, unit_scale=True, unit=" questions",
+                desc="Splitting data"):
+    story_id = row.story_id
+
+    # Filter out when no answer was picked because these weren't used in the original paper.
+    answer_char_ranges = row.answer_char_ranges.split('|')
+    none_count = answer_char_ranges.count('None')
+    if none_count == len(answer_char_ranges):
+        continue
     if story_id in train_story_ids:
         train_data.append(row)
     elif story_id in dev_story_ids:
@@ -47,14 +57,17 @@ for _, row in tqdm(original.iterrows(), total=len(original),
         logging.warning(
             "%s is not in train, dev, nor test", story_id)
 
-train_out = pd.DataFrame(data=train_data)
-dev_out = pd.DataFrame(data=dev_data)
-tests_out = pd.DataFrame(data=test_data)
-
 if not os.path.exists(args.output_dir):
     os.makedirs(args.output_dir)
 
+
+def _write_to_csv(data, path):
+    pd.DataFrame(data=data).to_csv(path,
+                                   columns=original.columns.values,
+                                   index=False, encoding='utf-8')
+
+
 logging.info("Writing split data to %s", args.output_dir)
-train_out.to_csv(os.path.join(args.output_dir, 'train.csv'), index=False, encoding='utf-8')
-dev_out.to_csv(os.path.join(args.output_dir, 'dev.csv'), index=False, encoding='utf-8')
-tests_out.to_csv(os.path.join(args.output_dir, 'test.csv'), index=False, encoding='utf-8')
+_write_to_csv(train_data, os.path.join(args.output_dir, 'train.csv'))
+_write_to_csv(dev_data, os.path.join(args.output_dir, 'dev.csv'))
+_write_to_csv(test_data, os.path.join(args.output_dir, 'test.csv'))
